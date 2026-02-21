3f5517c7db3bb2c785e0c016ede969dc1ef258e0
hojooneum <bldolphin96@gmail.com>
2026-02-21 22:58:14 +0900

    Update theme to everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,4 +8,15 @@ return {
       vim.cmd.colorscheme("catppuccin")
     end,
   },
+  {
+      'sainnhe/everforest',
+      lazy = false,
+      priority = 1001,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.everforest_enable_italic = false
+        vim.cmd.colorscheme('everforest')
+      end
+  }
 }
