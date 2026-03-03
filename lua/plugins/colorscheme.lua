489fd2d2ae4dbab26e1d61fdc74e5aa0e31eca95
Hojoon Eum <bldolphin96@gmail.com>
2026-03-03 23:52:07 +0900

    remove catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,10 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      -- Set the colorscheme
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-  {
       'sainnhe/everforest',
       lazy = false,
-      priority = 1001,
       config = function()
         -- Optionally configure and load the colorscheme
         -- directly inside the plugin declaration.
         vim.g.everforest_enable_italic = false
         vim.cmd.colorscheme('everforest')
       end
-  }
 }
