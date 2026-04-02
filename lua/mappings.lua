d646e4ca9c2c6dd816166c72951e9510d9194b6f
Hojoon Eum <bldolphin96@gmail.com>
2026-04-02 23:03:10 +0900

    update to follow nvchad2.5 practice

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,5 +4,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
-map("n", "<C-a>", "<C-^>", { noremap = true, silent = true, desc = "Switch to alternate file" })
-map("n", "<leader>o", "<cmd>Telescope oldfiles<CR>", { noremap = true, silent = true, desc = "Open recent files" })
+map("n", "<C-a>", "<C-^>", { desc = "Switch to alternate file" })
+map("n", "<leader>o", "<cmd>Telescope oldfiles<CR>", { desc = "Open recent files" })
