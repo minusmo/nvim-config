d405784f08b6c362cc87b985e36022f72085734a
Hojoon Eum <bldolphin96@gmail.com>
2026-04-02 23:16:34 +0900

    seprate lspconfigs

---
+++ lua/configs/lsp/emmet_ls.lua
@@ -0,0 +1,4 @@
+vim.lsp.config("emmet_ls", {
+  filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescriptreact" },
+})
+vim.lsp.enable("emmet_ls")
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,9 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "gopls", "ty", "postgres-language-server", "basedpyright", "emmet_ls" }
-vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers 
+require "configs.lsp.html"
+require "configs.lsp.cssls"
+require "configs.lsp.gopls"
+require "configs.lsp.ty"
+require "configs.lsp.postgres_lsp"
+require "configs.lsp.basedpyright"
+require "configs.lsp.emmet_ls"
