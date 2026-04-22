186209a3a3390b3f274213c52d7f538af93503f8
Hojoon Eum <bldolphin96@gmail.com>
2026-04-22 23:07:14 +0900

    20260422

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,40 +2,40 @@
 local M = {}
 
 M.base46 = {
-  theme = "gruvbox",
+    theme = "everforest_light",
 }
 
 M.ui = {
-  statusline = {
-    theme = "minimal",
-    separator_style = "default",
-    order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
-    modules = {
-      file = function()
-        local path = vim.fn.expand "%:~:."
-        if path == "" then
-          return "%#StText# 󰈚 Empty "
-        end
+    statusline = {
+        theme = "minimal",
+        separator_style = "default",
+        order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+        modules = {
+            file = function()
+                local path = vim.fn.expand "%:~:."
+                if path == "" then
+                    return "%#StText# 󰈚 Empty "
+                end
 
-        local filename = vim.fn.expand "%:t"
-        local ext = vim.fn.expand "%:e"
-        local devicons_ok, devicons = pcall(require, "nvim-web-devicons")
+                local filename = vim.fn.expand "%:t"
+                local ext = vim.fn.expand "%:e"
+                local devicons_ok, devicons = pcall(require, "nvim-web-devicons")
 
-        local icon = "󰈚 "
-        if devicons_ok then
-          local ft_icon = devicons.get_icon(filename, ext)
-          if ft_icon then
-            icon = ft_icon .. " "
-          end
-        end
+                local icon = "󰈚 "
+                if devicons_ok then
+                    local ft_icon = devicons.get_icon(filename, ext)
+                    if ft_icon then
+                        icon = ft_icon .. " "
+                    end
+                end
 
-        return "%#StText# " .. icon .. path .. " "
-      end,
-      lsp = function()
-        return require("configs.statusline").lsp()
-      end,
+                return "%#StText# " .. icon .. path .. " "
+            end,
+            lsp = function()
+                return require("configs.statusline").lsp()
+            end,
+        },
     },
-  },
 }
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,30 +1,31 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        css = { "prettier" },
-        html = { "prettier" },
-      },
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+                css = { "prettier" },
+                html = { "prettier" },
+            },
+        },
     },
-  },
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim", "lua", "vimdoc",
-        "html", "css",
-        "markdown", "markdown_inline",
-      },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
     },
-  },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        branch = "master",
+        opts = {
+            ensure_installed = {
+                "vim", "lua", "vimdoc",
+                "html", "css",
+            },
+        },
+    },
 }
