d6f374804b17c2a460142f207bb2c3d90c38c6ab
Hojoon Eum <bldolphin96@gmail.com>
2026-04-27 23:58:58 +0900

    rewind

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.opt.background = "light"
+      vim.opt.background = "dark"
       vim.g.everforest_background = "soft"
       vim.g.everforest_better_performance = 1
       vim.g.everforest_enable_italic = 1
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,33 +1,48 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo", "TSUpdate", "TSUpdateSync" },
-    opts = {
-      ensure_installed = {
-        "vim",
-        "vimdoc",
-        "lua",
-        "luadoc",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "go",
-        "python",
-        "sql",
-        "json",
-        "yaml",
-        "toml",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
+        cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo", "TSUpdate", "TSUpdateSync" },
+
+        -- main 브랜치에서는 configs.setup이 없어졌으므로 opts/config 분리 대신 직접 setup 호출
+        config = function()
+            -- 파서 설치 위치 (원하면 커스터마이즈 가능)
+            local ts = require("nvim-treesitter")
+            ts.setup({
+                -- parsers 설치 설정
+                ensure_installed = {
+                    "vim",
+                    "vimdoc",
+                    "lua",
+                    "luadoc",
+                    "html",
+                    "css",
+                    "markdown",
+                    "markdown_inline",
+                    "bash",
+                    "go",
+                    "python",
+                    "sql",
+                    "json",
+                    "yaml",
+                    "toml",
+                },
+                -- 필요시 auto_install 등 새 옵션 추가
+                auto_install = true,
+            })
+
+            -- 하이라이트/인덴트는 vim.treesitter API 쪽으로 이동
+            -- 전체 버퍼에 대해 자동으로 treesitter를 켜는 예시
+            vim.api.nvim_create_autocmd("FileType", {
+                group = vim.api.nvim_create_augroup("treesitter-main", { clear = true }),
+                pattern = "*",
+                callback = function(args)
+                    -- pcall로 감싸서 파서 없는 경우에도 에러 없이 넘어가게
+                    pcall(vim.treesitter.start, args.buf)
+                end,
+            })
+        end,
     },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
 }
